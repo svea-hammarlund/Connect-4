025937de83e1f1563ac9dfe350350b49d517fd6d
svea-hammarlund <153454967+svea-hammarlund@users.noreply.github.com>
2025-03-07 12:14:28 -0600

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,13 +1,6 @@
 package hw3;
 
 public class Board {
-	// TODO
-	// Add whatever private fields you need here.
-	// Remember, only variables of type int, char, boolean, and 1D and 2D arrays
-	// of these types are allowed.
-	//
-	// As always, you may also add private helper methods to the class.  That will
-	// likely be very useful on this assignment.
 	private char[][] board;
 	private char currentPlayer;
 	private boolean gameOver;
@@ -100,25 +93,10 @@ public class Board {
 		gameOver = false;
 	}
 
-	/**
-	 * Gets the current player (either 'X' or 'O')
-	 * 
-	 * @return the current player
-	 */
 	public char currentPlayer() {
 		return currentPlayer;
 	}
 
-	/**
-	 * The current player makes a move in a given column if it is a valid move.
-	 * Throws an exception if the game is already over.
-	 * 
-	 * @param column the column in which to make a move.  For the move to be valid,
-	 * the column value must an {@code int} between 1 and 7 inclusive, and
-	 * there must have been fewer than 6 moves already made in the given column.
-	 * @return {@code true} if the move is valid and false if it is not valid.
-	 * @throws RuntimeException if the game is already over.
-	 */
 	public boolean play(int col) {
 		if (gameStatus() !='U') {
 			throw new RuntimeException("The game has already ended.");
@@ -138,12 +116,6 @@ public class Board {
 		return true;
 	}
 
-	/**
-	 * Determine the status of the game.
-	 * 
-	 * @return {@code 'X'} or {@code 'O'} if either player has won, {@code 'D'} if
-	 * the game is a draw, and {@code 'U'} if the game is still undecided.
-	 */
 	public char gameStatus() {
 		for (int row = 0; row < 6; row++) {
 			for (int col = 0; col < 7; col++) {
@@ -158,12 +130,6 @@ public class Board {
 		return 'U';
 	}
 
-	/**
-	 * Construct a string that depicts the sate of the game.
-	 * (See the writeup for what that string should look like.)
-	 * 
-	 * @return a string depicting the game board
-	 */
 	public String toString() {
 		String result = "";
 		for (int row = 0; row < 6; row++) {
